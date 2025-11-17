41f6eb7c9299c74facaf1ba11be2e42909109a55
geeky-vaiiib <geeky.vaiiib28@gmail.com>
2025-11-17 11:30:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,31 @@ import { About } from "./components/sections/About.jsx";
 import { Projects } from "./components/sections/Projects.jsx";
 import { Contact } from "./components/sections/Contact.jsx";
 import { Footer } from "./components/sections/Footer.jsx";
+import Ribbons from "./components/Ribbons.jsx";
 
 export default function App() {
   return (
     <div className="min-h-screen">
+      {/* Full-screen ribbon cursor effect */}
+      <div style={{ 
+        position: 'fixed', 
+        top: 0, 
+        left: 0, 
+        width: '100%', 
+        height: '100%', 
+        pointerEvents: 'none',
+        zIndex: 9999
+      }}>
+        <Ribbons
+          baseThickness={30}
+          colors={['#ffffff']}
+          speedMultiplier={0.5}
+          maxAge={500}
+          enableFade={false}
+          enableShaderEffect={true}
+        />
+      </div>
+      
       <Navigation />
       <main id="main-content">
         <Hero />
